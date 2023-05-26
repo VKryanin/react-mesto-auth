7de9f0102e516e5d8817f12165939247a4382fd4
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2023-05-26 15:53:29 +0300

    rework

--- src/components/ProtectedRoute.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
-
-export const ProtectedRoute = ({ element: Component, ...props }) => {
-  const { pathname } = useLocation();
-  return props.loggedIn ? <Component {...props} /> : <Navigate to='/signin' state={{ backUrl: pathname }} />;
-}
