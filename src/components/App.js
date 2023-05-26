a17119ffd35fedc910f44dcd46639beae48f866c
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2023-05-26 15:41:13 +0300

    start

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { Main } from "./Main";
 import { ImagePopup } from "./ImagePopup";
@@ -6,15 +7,7 @@ import { PopupEditAvatar } from "./PopupEditAvatar";
 import { PopupEditProfile } from "./PopupEditProfile";
 import { PopupAddCard } from "./PopupAddCard";
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
-import { Footer } from "./Footer";
 import { api } from "../utils/Api";
-import { Routes, Route, Navigate, useNavigate, Link } from "react-router-dom";
-import { Signin } from "./Signin";
-import { Signup } from "./Signup";
-import { ProtectedRoute } from "./ProtectedRoute";
-import { InfoTooltip } from "./InfoTooltip";
-import * as mestoAuth  from '../utils/MestoAuth'
-
 
 function App() {
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
@@ -24,14 +17,6 @@ function App() {
   const [selectedCard, setSelectedCard] = useState({});
   const [cards, setCards] = useState([]);
   const [currentUser, setCurrentUser] = useState({});
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [waitingLoad, setWaitingLoad] = useState(true);
-  const navigate = useNavigate();
-  const [isInfoTooltipPopupOpen, setIsInfoTooltipPopupOpen] = useState(false);
-  const [successAuth, setSuccessAuth] = useState({
-    message: '',
-    selector: '',
-  });
 
   useEffect(() => {
     Promise.all([api.getProfile(), api.getCards()])
@@ -81,6 +66,7 @@ function App() {
 
   // доб. карточки
   function handleAddCard(cardItem) {
+    console.log(cardItem);
     api.addNewCard(cardItem.name, cardItem.link)
       .then((card) => { setCards([card, ...cards]); closeAllPopups() })
       .catch((err) => { console.log(`Возникла ошибка при добавлении новой карточки, ${err}`) })
@@ -104,96 +90,39 @@ function App() {
     setIsAddPlacePopupOpen(false);
     setIsImageOpen(false);
   }
-
-  const handleLogin = (inputValues) => {
-    mestoAuth.authorize(inputValues)
-    .then((res) => {
-      localStorage.setItem('jwt', res.token);
-      setLoggedIn(true);
-    })
-    .catch((err) => {
-      setSuccessAuth({
-        message: 'Что-то пошло не так! Попробуйте ещё раз.'
-      });
-      setIsInfoTooltipPopupOpen(true);
-    });
-  }
-
-  function handleExit() {
-    localStorage.clear()
-    setLoggedIn(false)
-  }
-
-  const handleRegister = (inputValues) => {
-    mestoAuth.register(inputValues)
-    .then(()=> {
-      setSuccessAuth({
-        message: 'Вы успешно зарегистрировались!'
-      });
-      setIsInfoTooltipPopupOpen(true);
-      navigate('/signin');
-    })
-    .catch((err) => {
-      setSuccessAuth({
-        message: 'Что-то пошло не так! Попробуйте ещё раз.'
-      });
-      setIsInfoTooltipPopupOpen(true);
-    })
-  }
-
   return (
-    <CurrentUserContext.Provider value={currentUser}>
-
-      <Routes>
-        <Route path="/" element={<Header />}>
-          <Route index element={
-            <Link to="/signin" className="auth-forms__link" onClick={handleExit}>
-              <p className="auth-forms__email">{currentUser.email}</p>
-              <p className="auth-forms__email">Выйти</p>
-            </Link>
-          } />
-          <Route path="signin" element={
-            <Link to="/signup" className="auth-forms__link">
-              Зарегистрироваться
-            </Link>
-          } />
-          <Route path="signup" element={
-            <Link to="/signin" className="auth-forms__link">
-              Войти
-            </Link>
-          } />
-        </Route>
-      </Routes>
-
-      <Routes>
-        <Route path="/" element={!waitingLoad && <ProtectedRoute
-          component={Main}
-          loggedIn={loggedIn}
-          handlers={{
-            onEditProfile: handleEditProfileClick,
-            handleAddCard: handleAddCard,
-            onEditAvatar: handleEditAvatarClick,
-            onCardClick: handleCardClick,
-            onCardLike: handleCardLike,
-            onCardDelete: handleCardDelete
-          }}
-          cards={cards}
-        />}
-        />
-        <Route path="/signin" element={<Signin handleLogin={handleLogin} />} />
-        <Route path="/signup" element={<Signup onRegister={handleRegister} />} />
-        <Route path="*" element={loggedIn ? <Navigate to="/" /> : <Navigate to="/signin" />} />
-      </Routes>
-      <Footer />
-
-      <PopupEditProfile isOpen={isEditProfilePopupOpen} onClose={closeAllPopups} onUpdateUser={handleUpdateUser} />
-      <PopupAddCard isOpen={isAddPlacePopupOpen} onClose={closeAllPopups} onAddPlace={handleAddCard} />
-      <PopupEditAvatar isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups} onUpdateAvatar={handleUpdateAvatar} />
-      <ImagePopup card={selectedCard} onClose={closeAllPopups} />
-      <InfoTooltip isOpen={isInfoTooltipPopupOpen} onClose={closeAllPopups} success={successAuth} />
-
+    < CurrentUserContext.Provider value={currentUser} >
+      <>
+        < Header />
+        < Main
+          onEditAvatar={handleEditAvatarClick}
+          onEditProfile={handleEditProfileClick}
+          onAddPlace={handleAddPlaceClick}
+          onCardClick={handleCardClick}
+          onCardDelete={handleCardDelete}
+          onCardLike={handleCardLike}
+          cards={cards} />
+        < Footer />
+        < ImagePopup
+          isOpen={isImageOpen}
+          onClose={closeAllPopups}
+          card={selectedCard} />
+        < PopupEditAvatar
+          isOpen={isEditAvatarPopupOpen}
+          onClose={closeAllPopups}
+          onUpdateAvatar={handleUpdateAvatar} />
+        < PopupEditProfile
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}
+          onUpdateUser={handleUpdateUser} />
+        < PopupAddCard
+          isOpen={isAddPlacePopupOpen}
+          onClose={closeAllPopups}
+          onAddPlace={handleAddCard} />
+      </>
     </CurrentUserContext.Provider>
-  )
+  );
 }
+
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,9 @@
 import logo from '../images/header__logo.svg'
 
-export function Header({handleEvent: click, title: title}) {
+export function Header() {
     return (
         <header className="header">
             <img className="header__logo" src={logo} alt='Лого "Mesto Russia"' />
-            <div className='header__container'>
-                <h3 className='header__user'>{localStorage.getItem('user')}</h3>
-                <button className='header__button' onClick={click}>{title}</button>
-            </div>
-
         </header>
     )
 }
